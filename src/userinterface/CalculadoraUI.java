b6d6833b10d667ec2dd764ce2632bf179409d991
Vinicius Camargo Andrade <vinicius.andrade1990@gmail.com>
2024-09-11 16:29:32 -0300

    v1.0.1 - Exercicio 1 testes automatizados

--- src/userinterface/CalculadoraUI.java
+++ src/userinterface/CalculadoraUI.java
@@ -7,15 +7,21 @@ import service.DivisaoImpossivelException;
 
 public class CalculadoraUI {
 
+	private static Scanner input;
+	private static double x;
+	private static double y;
+
 	public static void main(String[] args) {
 
-		try {
+		input = new Scanner(System.in);
+		CalculadoraService calcService = new CalculadoraService();
 
-			Scanner input = new Scanner(System.in);
-			CalculadoraService calcService = new CalculadoraService();
-			int opcao;
+		double resultado = 0.0;
+		int opcao = 0;
 
-			do {
+		do {
+			
+			try {
 
 				System.out.println("1 - Adição");
 				System.out.println("2 - Subtração");
@@ -27,46 +33,54 @@ public class CalculadoraUI {
 				opcao = input.nextInt();
 
 				if (opcao == 0)
-					break;
-
-				System.out.print("Informe o valor de x: ");
-				double x = input.nextDouble();
-
-				System.out.print("Informe o valor de y: ");
-				double y = input.nextDouble();
-
-				double resultado = 0.0;
+					continue;
 
 				switch (opcao) {
 
-					case 1:
-						resultado = calcService.somar(x, y);
-						break;
-	
-					case 2:
-						resultado = calcService.subtrair(x, y);
-						break;
-	
-					case 3:
-						resultado = calcService.multiplicar(x, y);
-						break;
-	
-					case 4:
-						resultado = calcService.dividir(x, y);
-						break;
-	
-					default:
-						System.out.println("Opção inválida.");
-						continue;
+				case 1:
+					obterValores();
+					resultado = calcService.somar(x, y);
+					break;
+
+				case 2:
+					obterValores();
+					resultado = calcService.subtrair(x, y);
+					break;
+
+				case 3:
+					obterValores();
+					resultado = calcService.multiplicar(x, y);
+					break;
+
+				case 4:
+					obterValores();
+					resultado = calcService.dividir(x, y);
+					break;
+
+				default:
+					System.out.println("Opção inválida.");
+					continue;
 				}
 
 				System.out.println("Resultado = " + resultado);
 
-			} while (opcao != 0);
+			} catch (DivisaoImpossivelException nnne) {
 
-		} catch (DivisaoImpossivelException nnne) {
+				System.err.println(nnne.getMessage());
+			}
+			
+		} while (opcao != 0);
 
-			System.err.println(nnne.getMessage());
-		}
+		System.out.println("Finalizado.");
+
+	}
+
+	private static void obterValores() {
+
+		System.out.print("Informe o valor de x: ");
+		x = input.nextDouble();
+
+		System.out.print("Informe o valor de y: ");
+		y = input.nextDouble();
 	}
 }
